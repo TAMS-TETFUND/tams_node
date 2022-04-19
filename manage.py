781b610b4daf247803156a870d0a9dfeba6edc10
mebi36 <chukwudumebinwobi@gmail.com>
2022-04-19 04:23:04 +0100

    add modified manage.py file

--- manage.py
+++ manage.py
@@ -5,27 +5,29 @@ from django.core.management import execute_from_command_line
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+
 def init_django():
     if settings.configured:
         return
-    
+
     settings.configure(
         INSTALLED_APPS=[
-            'django.contrib.auth',
-	    	'django.contrib.contenttypes',
-            'db'
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+            "db",
         ],
         DATABASES={
-            'default':{
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
             }
         },
-        AUTH_USER_MODEL = 'db.AppUser',
-        DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+        AUTH_USER_MODEL="db.AppUser",
+        DEFAULT_AUTO_FIELD="django.db.models.BigAutoField",
     )
     django.setup()
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     init_django()
     execute_from_command_line()
