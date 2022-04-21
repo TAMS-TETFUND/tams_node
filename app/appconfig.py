285835214fb56d6168c00ed0c8d3b1d84fb778c2
mebi36 <chukwudumebinwobi@gmail.com>
2022-04-21 06:52:54 +0100

    refactored the configparser for the project into a class

---
+++ app/appconfig.py
@@ -0,0 +1,21 @@
+import configparser
+import os
+from pathlib import Path
+
+
+class AppConfigParser(configparser.ConfigParser):
+    """An extension of the standard library's ConfigParser class for
+    the project.
+    """
+
+    CONFIG_FILE = os.path.join(
+        Path(os.path.abspath(__file__)).parent, "config.ini"
+    )
+
+    def __init__(self):
+        super(AppConfigParser, self).__init__()
+        self.read(self.CONFIG_FILE)
+
+    def save_config(self):
+        with open(self.CONFIG_FILE, "w") as configfile:
+            self.write(configfile)
