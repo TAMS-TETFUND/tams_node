9122db3351ab562740568162d81803625dbbdc86
mebi36 <chukwudumebinwobi@gmail.com>
2022-04-23 19:25:51 +0100

    changed instantiation of camera class to a context manager

--- app/gui.py
+++ app/gui.py
@@ -608,15 +608,14 @@ class CameraWindow(BaseGUIWindow):
     @staticmethod
     def loop(window, event, values):
         global window_dispatch
-        cam = Camera()
         cam_on = True
-
-        while cam_on:
-            event, values = window.read(timeout=20)
-            if event == "capture":
-                window_dispatch["camera_win"].close()
-                return True
-            window["image_display"].update(data=cam.feed())
+        with Camera() as cam:
+            while cam_on:
+                event, values = window.read(timeout=20)
+                if event == "capture":
+                    window_dispatch["camera_win"].close()
+                    return True
+                window["image_display"].update(data=cam.feed())
         return True
 
 
