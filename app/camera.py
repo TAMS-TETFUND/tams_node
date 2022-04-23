39c8a95339a2fd0c911d2096e94016b3b0642319
mebi36 <chukwudumebinwobi@gmail.com>
2022-04-23 10:19:10 +0100

    corrected error of overriding the parent class's capture method

--- app/camera.py
+++ app/camera.py
@@ -1,24 +1,19 @@
+import time
+
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import cv2
+import numpy as np
 
 class Camera(PiCamera):
     def __init__(self):
         super(Camera, self).__init__()
-        self.resolution = (640, 480)
-        self.framerate = 32
+        self.resolution = (320, 240)
+        self.framerate = 24
+        time.sleep(2)
 
-    @property
-    def raw_capture(self, size=None):
-        if size is None:
-            size = self.resolution
-        return PiRGBArray(self, size)
+    def feed(self, format="bgr", use_video_port=True):
+            output = np.empty((240, 320, 3), dtype=np.uint8)
+            self.capture(output, 'rgb', use_video_port=True)
 
-    def capture(self, format="bgr", use_video_port=True):
-        for frame in self.capture_continuous(self.raw_capture, format=format, use_video_port=use_video_port):
-            # image = frame.array
-            return cv2.imencode('.png', frame)[1].tobytes()
-
-
-            
-    
+            return cv2.imencode('.png', output)[1].tobytes()
