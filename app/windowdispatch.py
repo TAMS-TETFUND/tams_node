414a82e748fe9f4e1d4fe0a0591868c7a0677efd
mebi36 <chukwudumebinwobi@gmail.com>
2022-04-25 10:26:23 +0100

    removed the unnecessary window loop method and added annotation to the windowdispatch module

--- app/windowdispatch.py
+++ app/windowdispatch.py
@@ -1,4 +1,9 @@
 from collections import UserDict
+from typing import Type
+
+import PySimpleGUI as sg
+
+from app.basegui import BaseGUIWindow
 
 
 class WindowDispatch(UserDict):
@@ -6,7 +11,7 @@ class WindowDispatch(UserDict):
     def __init__(self, *args, **kwargs):
         super(WindowDispatch, self).__init__(*args, **kwargs)
 
-    def open_window(self, window_class, on_seperate_window=False):
+    def open_window(self, window_class: Type[BaseGUIWindow], on_seperate_window : bool = False) -> sg.Window:
         self.update({window_class.__name__: window_class.window()})
 
         if not on_seperate_window:
@@ -15,12 +20,8 @@ class WindowDispatch(UserDict):
                 if key != window_class.__name__:
                     self.close_window(key)
     
-    def close_window(self, window_name):
+    def close_window(self, window_name:str):
         return self.pop(window_name).close()
 
-    def find_window(self, window_object):
+    def find_window(self, window_object:sg.Window):
         return next((k for k, v in self.items() if v == window_object), None)
-
-    @staticmethod
-    def window_loop(window_name, window, event, values):
-        return eval(f"{window_name}.loop(window, event, values)")
