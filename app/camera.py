f3a6569607459e4ef9783b4659bb3fc3ef9a0076
mebi36 <chukwudumebinwobi@gmail.com>
2022-05-03 11:28:24 +0100

    refactored feed method into two methods

--- app/camera.py
+++ app/camera.py
@@ -7,16 +7,20 @@ import numpy as np
 
 
 class Camera(PiCamera):
-    def __init__(self):
+    def __init__(self) -> None:
         super(Camera, self).__init__()
         self.resolution = (320, 240)
         self.framerate = 24
-        time.sleep(2)
+        time.sleep(1)
 
-    def feed(self, format="bgr", use_video_port=True):
+    def feed(self, format="bgr", use_video_port=True) -> np.array:
         output = np.empty((240, 320, 3), dtype=np.uint8)
         self.capture(output, "bgr", use_video_port=True)
-        return cv2.imencode(".png", output)[1].tobytes()
+        return output
+
+    @staticmethod
+    def feed_tobytes(img):
+        return cv2.imencode(".png", img)[1].tobytes()
 
     def save_feed(
         self, filename="demo_img", path=os.path.dirname("demo_images/")
