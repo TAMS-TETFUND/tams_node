d08d534c610116e089f08f5bef6eb5a5eff8fc6c
mebi36 <chukwudumebinwobi@gmail.com>
2022-05-07 05:55:17 +0100

    adust image dimension to fit window size

--- app/camera.py
+++ app/camera.py
@@ -22,9 +22,9 @@ class Camera(PiCamera):
     @staticmethod
     def feed_to_bytes(img):
         print(img.shape)
-        print(bgw.SCREEN_SIZE)
+        new_image_size = tuple(x * 0.5 for x in bgw.SCREEN_SIZE)
         return cv2.imencode(".png", 
-            cv2.resize(img, dsize=(x * 0.5 for x in bgw.SCREEN_SIZE), fx=0.6, fy=0.6, interpolation=cv2.INTER_AREA)
+            cv2.resize(img, dsize=new_image_size, fx=0.6, fy=0.6, interpolation=cv2.INTER_AREA)
             # cv2.resize(img, dsize=img.shape[:2], fx=0.6, fy=0.6, interpolation=cv2.INTER_AREA)
             # img[::2, ::2]            
         )[1].tobytes()
