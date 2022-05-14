10ce09f0265474c57e4c360a1761a01a1d0935b3
mebi36 <chukwudumebinwobi@gmail.com>
2022-05-14 18:15:49 +0100

    added sudo privilege for editing system files

--- hotspot_config.sh
+++ hotspot_config.sh
@@ -9,15 +9,15 @@ sudo apt install -y dnsmasq
 
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 
-touch /etc/dhcpcd.conf
+sudo touch /etc/dhcpcd.conf
 
-echo "interface wlan0" >> /etc/dhcpcd.conf
-echo "  static ip_address=192.168.4.1/24" >> /etc/dhcpcd.conf
-echo "  nohook wpa_supplicant" >> /etc/dhcpcd.conf
+sudo echo "interface wlan0" >> /etc/dhcpcd.conf
+sudo echo "  static ip_address=192.168.4.1/24" >> /etc/dhcpcd.conf
+sudo echo "  nohook wpa_supplicant" >> /etc/dhcpcd.conf
 
-touch /etc/sysctl.d/routed-ap.conf
+sudo touch /etc/sysctl.d/routed-ap.conf
 
-echo "net.ipv4.ip_forward=1"
+sudo echo "net.ipv4.ip_forward=1"
 
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
@@ -25,23 +25,23 @@ sudo netfilter-persistent save
 
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 
-touch /etc/dnsmasq.conf
+sudo touch /etc/dnsmasq.conf
 
-echo "interface=wlan0" >> /etc/dnsmasq.conf
-echo "dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h" >> /etc/dnsmasq.conf
-echo "domain=wlan" >> /etc/dnsmasq.conf
-echo "address=/gw/wlan/192.168.4.1" >> /etc/dnsmasq.conf
+sudo echo "interface=wlan0" >> /etc/dnsmasq.conf
+sudo echo "dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h" >> /etc/dnsmasq.conf
+sudo echo "domain=wlan" >> /etc/dnsmasq.conf
+sudo echo "address=/gw/wlan/192.168.4.1" >> /etc/dnsmasq.conf
 
-touch /etc/hostapd/hostapd.conf
+sudo touch /etc/hostapd/hostapd.conf
 
-echo "interface=wlan0" >> /etc/hostapd.conf
-echo "ssid=tams01" >> /etc/hostapd.conf
-echo "hw=mode=g" >> /etc/hostapd.conf
-echo "channel=7" >> /etc/hostapd.conf
-echo "auth_algs=1" >> /etc/hostapd.conf
-echo "ignore_broadcast_ssid=0" >> /etc/hostapd.conf
-echo "wpa=2" >> /etc/hostapd.conf
-echo "wpa_passphrase=tams01" >> /etc/hostapd.conf
-echo "wpa_key_mgmt" >> /etc/hostapd.conf
-echo "wpa_pairwise=TKIP" >> /etc/hostapd.conf
-echo "rsn_pairwise=CCMP" >> /etc/hostapd.conf
+sudo echo "interface=wlan0" >> /etc/hostapd.conf
+sudo echo "ssid=tams01" >> /etc/hostapd.conf
+sudo echo "hw=mode=g" >> /etc/hostapd.conf
+sudo echo "channel=7" >> /etc/hostapd.conf
+sudo echo "auth_algs=1" >> /etc/hostapd.conf
+sudo echo "ignore_broadcast_ssid=0" >> /etc/hostapd.conf
+sudo echo "wpa=2" >> /etc/hostapd.conf
+sudo echo "wpa_passphrase=tams01" >> /etc/hostapd.conf
+sudo echo "wpa_key_mgmt" >> /etc/hostapd.conf
+sudo echo "wpa_pairwise=TKIP" >> /etc/hostapd.conf
+sudo echo "rsn_pairwise=CCMP" >> /etc/hostapd.conf
