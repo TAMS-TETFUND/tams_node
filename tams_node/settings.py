aa9213a1be69df9a4a2c78e8d364803c18944114
mebi36 <chukwudumebinwobi@gmail.com>
2022-05-15 14:59:48 +0100

    rearranged order of installed apps to be able to modify logged_out.html for the projects

--- tams_node/settings.py
+++ tams_node/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'db',
+    'app',
+    'webapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'db',
-    'app',
-    'webapp'
 ]
 
 MIDDLEWARE = [
