6a4c8e1b43309a2845a537517922f86570dc81fb
mebi36 <chukwudumebinwobi@gmail.com>
2022-05-16 21:06:30 +0100

    added save function to __setitem__ and remove_section methods of the application's configparser class

--- app/appconfigparser.py
+++ app/appconfigparser.py
@@ -7,7 +7,6 @@ class AppConfigParser(configparser.ConfigParser):
     """An extension of the standard library's ConfigParser class for
     the project.
     """
-
     CONFIG_FILE = os.path.join(
         Path(os.path.abspath(__file__)).parent,
         ("config.ini" if os.name != "posix" else ".config.ini"),
@@ -16,6 +15,15 @@ class AppConfigParser(configparser.ConfigParser):
     def __init__(self):
         super(AppConfigParser, self).__init__()
         self.read(self.CONFIG_FILE)
+
+    def __setitem__(self, key: str, value) -> None:
+        super().__setitem__(key, value)
+        self.save()
+
+    def remove_section(self, section: str) -> bool:
+        result = super().remove_section(section)
+        self.save()
+        return result
 
     def save(self):
         with open(self.CONFIG_FILE, "w") as configfile:
