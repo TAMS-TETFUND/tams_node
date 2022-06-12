f7dcdff0851c9ad1e2745fccadd61a1c542d76e7
mebi36 <chukwudumebinwobi@gmail.com>
2022-06-12 17:02:53 +0100

    added the face location resizing method

--- app/camerafacerec.py
+++ app/camerafacerec.py
@@ -4,6 +4,7 @@ from threading import Thread
 from app.camera2 import Camera
 from app.facerec import FaceRecognition
 
+
 class CamFaceRec:
     def __init__(self):
         self.cam = Camera()
@@ -15,7 +16,7 @@ class CamFaceRec:
     def _start_thread(self):
         Thread(target=self.face_read, args=()).start()
         return self
-    
+
     def _stop_thread(self):
         self.stopped = True
 
@@ -26,15 +27,19 @@ class CamFaceRec:
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.cam.stop()
         self._stop_thread()
-    
-    def start_deque(self):
+
+    def load_facerec_attrs(self):
         if self.deque_not_empty:
-            self.img, self.face_locations, self.face_count = self.attr_deque.pop()
+            (
+                self.img,
+                self.face_locations,
+                self.face_count,
+            ) = self.attr_deque.pop()
         else:
             raise RuntimeError("attr_deque empty")
 
     def refresh(self):
-        self.start_deque()
+        self.load_facerec_attrs()
 
     def face_read(self):
         self.img_bbox = self.cam.feed()
@@ -42,30 +47,36 @@ class CamFaceRec:
             if self.stopped:
                 return
             self._img = self._img_bbox = self.cam.feed()
-            self._face_locations = self.face_rec.face_locations(self._img)
+            self._face_locations = self.face_rec.face_locations(self.cam.reduce_framesize(self._img))
+            self._face_locations = self.scale_face_locations(self._face_locations)
 
             if len(self._face_locations) > 0:
                 for face_loc in self._face_locations:
                     self.face_rec.draw_bounding_box(face_loc, self._img_bbox)
             self.img_bbox = self._img_bbox
-            self.attr_deque.append([self._img, self._face_locations, len(self._face_locations)])
+            self.attr_deque.append(
+                [self._img, self._face_locations, len(self._face_locations)]
+            )
 
     def deque_not_empty(self):
         return True if len(self.attr_deque) > 0 else False
-    
-    # def face_count(self, image=None):
-    #     if image:
-    #         return len(self.face_rec.face_locations(image))
-    #     else:
-    #         return len(self.face_locations)
 
     def face_encodings(self):
         return self.face_rec.face_encodings(self.img, [self.face_locations[0]])
 
-# The issue here is that the state of the face_count method is
-# changing everytime the method is called
-# also the state of self.img is changing which means that self.locations
-# is also changing very rapidly?
-# it is unstable for the clients of the class to access these attributes
-# directly. A deque needs to exist to ensure that img, face_locations,
-#  img_bbox, and consequently face_encodings will be consistent.
+    @staticmethod
+    def scale_face_locations(face_locations):
+        """
+        Scale back up face locations since the frame when framesize is reduced
+        """
+        resized_face_locations = []
+        for face_loc in face_locations:
+            (top, right, bottom, left) = face_loc
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            resized_face_locations.append(tuple([top, right, bottom, left]))
+        
+        return resized_face_locations
