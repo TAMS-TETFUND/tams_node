b485b838a1ebc64112e41e274dbfb2eaa3980f74
mebi36 <chukwudumebinwobi@gmail.com>
2022-06-13 13:47:22 +0100

    commented out some installs

--- scripts/initial_device_installs.sh
+++ scripts/initial_device_installs.sh
@@ -21,27 +21,26 @@ sudo apt-get install -y python3-dev
 sudo apt-get install -y python3-numpy
 sudo apt-get install -y python3-pip
 sudo apt-get install -y zip
-sudo apt-get -y clean
 
-sudo apt-get install -y  python3-picamera
-sudo pip install --upgrade -y picamera[array]
-sudo pip install -r ../requirements.txt
+# sudo apt-get install -y  python3-picamera
+# sudo pip install --upgrade -y picamera[array]
+# sudo pip install -r ../requirements.txt
 
-sudo touch /etc/dphys-swapfile
+su#do touch /etc/dphys-swapfile
 
-sudo sed -i s'/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/' /etc/dphys-swapfile
+#sudo sed -i s'/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/' /etc/dphys-swapfile
 
-sudo pip install dlib
+sudo pip install dlib -vvv
 
-sudo sed -i s'/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/' /etc/dphys-swapfile
+#sudo sed -i s'/CONF_SWAPSIZE=1024/CONF_SWAPSIZE=100/' /etc/dphys-swapfile
 
-sudo pip install numpy
-sudo pip install scikit-image
-sudo apt-get install python3-scipy
-sudo apt-get install libatlas-base-dev
-sudo apt-get install libjasper-dev
-sudo apt-get install libqtgui4
-sudo apt-get install python3-pyqt5
-sudo apt install libqt4-test
-sudo pip install opencv-python==3.4.6.27
-sudo pip install face_recognition 
+# sudo pip install numpy
+# sudo pip install scikit-image
+# sudo apt-get install python3-scipy
+# sudo apt-get install libatlas-base-dev
+# sudo apt-get install libjasper-dev
+# sudo apt-get install libqtgui4
+# sudo apt-get install python3-pyqt5
+# sudo apt install libqt4-test
+# sudo pip install opencv-python==3.4.6.27
+# sudo pip install face_recognition 
