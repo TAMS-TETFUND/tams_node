ed920bc04d01325b95e29f9040aaefdac87206a8
mebi36 <chukwudumebinwobi@gmail.com>
2022-06-14 12:02:01 +0100

    added an IP address to ALLOWED_HOSTS for testing

--- tams_node/settings.py
+++ tams_node/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l08e84q_1#54j@$_u1xgtr3n15kp3(lktu01oc7nfln$4%%9a)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.128', 'localhost', '127.0.0.1']
 
 
 # Application definition
