1587cf9b996297422fb0a22cb3a0d6e70f78c0e4
mebi36 <chukwudumebinwobi@gmail.com>
2022-10-16 15:38:59 +0100

    added close confirmation

--- app/main.py
+++ app/main.py
@@ -2,9 +2,14 @@
 
 import PySimpleGUI as sg
 
+
+from manage import django_setup
+
+django_setup()
+
 import app.appconfigparser
 import app.windowdispatch
-from app.gui_utils import update_device_op_mode
+
 
 
 app_config = app.appconfigparser.AppConfigParser()
@@ -13,22 +18,24 @@ window_dispatch = app.windowdispatch.WindowDispatch()
 
 def main_loop() -> None:
     """Function that starts the application."""
-    window_dispatch.dispatch.open_window("HomeWindow")
+    import app.guiutils
+    
     # setting the operational mode of device
     app_config.cp["tmp_settings"] = {}
-    update_device_op_mode()
+    app.guiutils.update_device_op_mode()
     continue_loop = True
-    window = None
 
     while continue_loop:
-        window = window_dispatch.dispatch.current_window
+        window = window_dispatch.dispatch.app_window
         event, values = window.read(timeout=500)
-        current_window = window_dispatch.dispatch.find_window_name(window)
+        current_window = window_dispatch.dispatch.current_window
         current_window_class = window_dispatch.dispatch.current_window_class
         if current_window:
             continue_loop = current_window_class.loop(window, event, values)
-        if event == sg.WIN_CLOSED:
+        
+        if event == sg.WINDOW_CLOSE_ATTEMPTED_EVENT:
+            from app.basegui import BaseGUIWindow
+            continue_loop = BaseGUIWindow.confirm_exit()
+        
+        if event in (sg.WIN_CLOSED, None):
             break
-
-    if window is not None:
-        window.close()
